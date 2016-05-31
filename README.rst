f84983321692c6aabe2d763c0af65d2c1c8bca36
Baruch Sterin <baruchs@gmail.com>
2016-05-30 22:15:35 -0700

    build project using setup.py

---
+++ README.rst
@@ -0,0 +1,31 @@
+# PyZZ
+
+This repository combines the various tools and libraries needed for building [https://bitbucket.org/sterin/pyzz](pyzz).
+
+# Requirements
+
+* CMake 3.3 or above
+* Ninja build tool
+* g++ 4.8 or above (or clang with similar levels of C++11 support)
+* Python 2.7 with development headers and libraries
+* setuptools
+* Zlib header files and libraries
+* Mercurial
+
+For example, in Ubuntu 16.04, run the following command to satisfy all the requirements:
+
+    sudo apt-get install cmake ninja-build g++ python-dev python-setuptools mercurial zlib1g-dev
+
+Note that the version of CMake included in previous releases of Ubuntu is too old. This requires CMake 3.3 or above to be manually installed.
+
+# How to build this tool #
+
+    python setup.py build
+
+# How to install this tool
+
+    python setup.py install
+
+or (only for the current user)
+
+    python setup.py install --user
---
+++ setup.py
@@ -0,0 +1,81 @@
+import setuptools
+
+import os
+import sys
+import platform
+import subprocess
+import shutil
+
+from distutils.command.build_ext import build_ext as _build_ext
+from setuptools import setup, Extension
+
+source_dir = os.path.dirname(os.path.abspath(__file__))
+
+class build_ext(_build_ext):
+
+    def __init__(self, *args, **kwargs):
+        _build_ext.__init__(self, *args, **kwargs)
+
+    def initialize_options(self):
+        self.cmake_dir = None
+        self.install_dir = None
+        _build_ext.initialize_options(self)
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        self.cmake_dir = os.path.abspath(os.path.join(self.build_temp, 'cmake'))
+        self.install_dir =  os.path.join(self.cmake_dir, 'install_dir')
+        if not os.path.isdir(self.cmake_dir):
+            os.makedirs(self.cmake_dir)
+
+    def run(self):
+
+        cmake_options = [
+            'cmake',
+            '-G', 'Ninja',
+            '-DCMAKE_INSTALL_PREFIX=%s'%self.install_dir,
+            '-DPYTHON_EXECUTABLE=%s'%sys.executable,
+        ]
+
+        if self.debug:
+            cmake_options.append('-DCMAKE_BUILD_TYPE=Debug')
+        else:
+            cmake_options.append('-DCMAKE_BUILD_TYPE=Release')
+
+        cmake_options.append(source_dir)
+
+        subprocess.check_call(cmake_options, cwd=self.cmake_dir)
+        subprocess.check_call(['ninja'], cwd=self.cmake_dir)
+        subprocess.check_call(['ninja', 'install'], cwd=self.cmake_dir)
+
+        for ext in self.extensions:
+            src = "%s.so"%os.path.join(self.install_dir, 'lib', *ext.name.split('.'))
+            dst = self.get_ext_fullpath(ext.name)
+            if not os.path.isdir(os.path.dirname(dst)):
+                os.makedirs(os.path.dirname(dst))
+            shutil.copyfile(src, dst)
+
+setup(
+    name = "PyZZ",
+    version = "0.0.1",
+    description = "",
+    url="https://bitbucket.org/sterin/pyzz_build",
+    author='Baruch sterin',
+    author_email='pyzz@bsterin.com',
+    license="BSD-like",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'License :: Freely Distributable',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords = '',
+    packages = ['pyzz'],
+    package_dir={'pyzz':'pyzz/'},
+    include_package_data = True,
+    zip_safe = False,
+    cmdclass = dict(build_ext=build_ext),
+    ext_modules = [Extension('pyzz._pyzz', [])],
+)
