99a26a3b8911dbb08d80f31211c753d3fbaaf072
Baruch Sterin <baruchs@gmail.com>
2020-02-28 00:06:08 +0200

    fix order of arguments to bdist_wheel, bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class build_ext(_build_ext):
 
 setup(
     name = "pyzz",
-    version = "0.0.7",
+    version = "0.0.8",
     description = "",
     url="https://github.com/sterin/pyzz-build",
     author='Baruch Sterin',
